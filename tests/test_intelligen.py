0e317debcd8cb25d3f6e1db2659f218d87e704fc
Diego Bouchet <diegobouchet88@gmail.com>
2021-11-27 13:00:22 +0100

    init commit

---
+++ tests/test_intelligen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `intelligen` package."""
+
+
+import unittest
+
+from intelligen import intelligen
+
+
+class TestIntelligen(unittest.TestCase):
+    """Tests for `intelligen` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
