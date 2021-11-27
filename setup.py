52919f845b814af16f297d071805910df30e9707
Diego Bouchet <diegobouchet88@gmail.com>
2021-11-27 16:37:26 +0100

    Bump version: 0.8.0 → 0.8.1

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Bouchet07/intelligen',
-    version='0.8.0',
+    version='0.8.1',
     zip_safe=False,
 )
