c932dc9a05efb670652467be5a3f3edaf4146271
Diego Bouchet <diegobouchet88@gmail.com>
2022-04-03 21:19:34 +0200

    fortran added

--- intelligen/stats/distributions.py
+++ intelligen/stats/distributions.py
@@ -1,80 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
-#from math import factorial
+from .functions import erf
+
 from typing import List, Union
 Vector = List[float]
 
 from numpy import pi, sqrt
-from .numeric import factorial, combination
-from .integrate import trapz
+from ..numeric import factorial, combination
 
-
-
-#---------------------- Methods -----------------------#
-
-def mean_squared_error(y_real: Vector, y_pred: Vector) -> float:
-    """Returns the mean squared error
-    Args:
-        y_real (Vector): Real data
-        y_pred (Vector): Predicted data
-    Returns:
-        float: mean squared error
-    """
-    y_real = np.array(y_real)
-    y_pred = np.array(y_pred)
-    
-    return np.mean((y_real - y_pred)**2)
-def mean_absolute_error(y_real: Vector, y_pred: Vector) -> float:
-    """Returns the mean absolute error
-    Args:
-        y_real (Vector): Real data
-        y_pred (Vector): Predicted data
-    Returns:
-        float: mean absolute error
-    """
-    y_real = np.array(y_real)
-    y_pred = np.array(y_pred)
-    
-    return np.mean(np.abs(y_real - y_pred))
-
-
-def erf(x: Union[float, Vector]) -> Union[float, Vector]:
-    """
-    Error Function
-    ==============
-
-    Parameters
-    ----------
-    x : float, Vector
-        Value
-
-    Returns
-    -------
-    float, Vector
-        Error function at x
-    """
-    
-    if isinstance(x, list): return [math.erf(x) for x in x]
-    elif type(x) == np.ndarray: return np.array([math.erf(x) for x in x])
-    else: return math.erf(x)
-
-def erfc(x: Union[float, Vector]) -> Union[float, Vector]:
-    """
-    Complementary Error Function
-    ==============
-
-    Parameters
-    ----------
-    x : float, Vector
-        Value
-
-    Returns
-    -------
-    float, Vector
-        Complementary error function at x
-    """
-    return 1 - erf(x)
+__all__ = ['Distribution', 'DiscreteDistribution',
+           'Bernoulli', 'Binomial', 'Geometric',
+           'NegativeBinomial', 'Hypergeometric',
+           'Poisson', 'Normal']
 
 #---------------------- Distributions -----------------------#
 
