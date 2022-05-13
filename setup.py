0b6a2c7d59dc3559e155cd4a303a8a07ef0cdc10
Diego Bouchet <diegobouchet88@gmail.com>
2022-05-13 15:05:26 +0200

    Bump version: 0.12.21 → 0.13.0

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Bouchet07/intelligen',
-    version='0.12.21',
+    version='0.13.0',
     zip_safe=False,
 )
